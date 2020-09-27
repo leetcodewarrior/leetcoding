8cf10da55f3799c5152eda26168df477ccc420ee
leetcodewarrior <71842405+leetcodewarrior@users.noreply.github.com>
2020-09-27 09:03:31 -0700

    Update LC495TeemoAttacking.java

--- LC495TeemoAttacking.java
+++ LC495TeemoAttacking.java
@@ -13,20 +13,16 @@ class Solution {
         //     }
         // }
         return result + duration;
-//         int[] prev = null;
+//         int[] prev = new int[]{ timeSeries[0], timeSeries[0] + duration - 1};
         
-//         for(int index = 0; index < timeSeries.length; index++){
-//             if(prev == null){
-//                 prev = new int[]{ timeSeries[index], timeSeries[index] + duration - 1};
-//             }else{
-//                 int[] current = new int[]{timeSeries[index], timeSeries[index] + duration - 1};
-//                 if(prev[1] >= current[0]){
-//                     prev[0] = Math.min(prev[0], current[0]);
-//                     prev[1] = Math.max(prev[1], current[1]);
-//                 }else{
-//                     result += prev[1] - prev[0] + 1;
-//                     prev = current;
-//                 }
+//         for(int index = 1; index < timeSeries.length; index++){
+//              int[] current = new int[]{timeSeries[index], timeSeries[index] + duration - 1};
+//              if(prev[1] >= current[0]){
+//                 prev[0] = Math.min(prev[0], current[0]);
+//                 prev[1] = Math.max(prev[1], current[1]);
+//              }else{
+//                 result += prev[1] - prev[0] + 1;
+//                 prev = current;
 //             }
 //         }
 //         if(prev != null){
